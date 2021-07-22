f0416941a8698d7c8e03ceea37f1572f9c3d45f9
Matt Wright <mwright@cairnselectric.ca>
2021-07-22 13:21:20 -0700

    add cp

--- pours/src/main.rs
+++ pours/src/main.rs
@@ -1,8 +1,14 @@
 #![allow(dead_code, unused_imports, unused_variables)]
-use petgraph::Graph;
 use petgraph::algo::*;
-use petgraph::visit::{GraphBase, NodeRef};
 use petgraph::graph::NodeIndex;
+use petgraph::visit::{GraphBase, NodeRef};
+use petgraph::Graph;
+
+#[derive(Default, Debug)]
+struct Project {
+    tasks: Graph<Task, Task>,
+    ordered_tasks: Vec<NodeIndex>,
+}
 
 #[derive(Default, Debug)]
 struct Task {
@@ -11,26 +17,38 @@ struct Task {
 }
 
 fn main() {
-    let mut deps = Graph::<Task, Task>::new();
+    let mut deps = Graph::<_, Task>::new();
 
-    let c = deps.add_node(Task { name: "c".to_string(), duration: 0 });
-    let a = deps.add_node(Task { name: "a".to_string(), duration: 0 });
-    let b = deps.add_node(Task { name: "b".to_string(), duration: 0 });
+    let task_a = Task {
+        name: "c".to_string(),
+        duration: 0,
+    };
 
+    let task_b = Task {
+        name: "a".to_string(),
+        duration: 0,
+    };
 
-    deps.extend_with_edges(&[
-        (a, b), (a, c),
-    ]);
+    let task_c = Task {
+        name: "b".to_string(),
+        duration: 0,
+    };
+
+    let c = deps.add_node(task_a);
+    let a = deps.add_node(task_b);
+    let b = deps.add_node(task_c);
+
+    deps.extend_with_edges(&[(a, b), (a, c)]);
 
     // let sorted = petgraph::algo::toposort(&deps,None).unwrap();
     // deps.into_nodes_edges().0.into_iter().for_each(|n| println!("{:?}", n.weight));
 
-    let sorted: Vec<NodeIndex> = petgraph::algo::toposort(&deps,None).unwrap();
+    let sorted: Vec<NodeIndex> = petgraph::algo::toposort(&deps, None).unwrap();
     // sorted.into_iter().for_each(|i| println!("{:?}", deps.node_weight(i).unwrap()));
 
     // deps.node_weight_mut(sorted[0].name).unwrap() = &mut Task { name: "new a".to_string(), duration: 0 };
 
-    println!("{:?}",deps.node_weight(sorted[0]))
+    println!("{:?}", deps.node_weight(sorted[0]))
 
     // match toposort(&deps, None) {
     //     Ok(order) => {
